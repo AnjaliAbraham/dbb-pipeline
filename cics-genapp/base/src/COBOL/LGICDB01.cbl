ac833f7a9540660941ff74076d935198d1b52441
RegiBrazil <rbarosa@us.ibm.com>
2020-12-10 12:09:09 -0500

    * on  Dec 10 2020  12:08

--- cics-genapp/base/src/COBOL/LGICDB01.cbl
+++ cics-genapp/base/src/COBOL/LGICDB01.cbl
@@ -1,6 +1,6 @@
       ******************************************************************
       * Changed by Regi to demonstrate a BUG introduced
-      * on  Dec 10 2020  10:48
+      * on  Dec 10 2020  12:08
       *                    Inquire Customer      TRX SSC1
       * Select  customer details from DB2 table
       *  Look for %bug to introdude a  bug
